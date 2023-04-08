d3b657c07144603801e133e4c309cb604e462e33
munozarturo <munoz.arturoroman@gmail.com>
2023-04-08 19:03:57 -0400

    added table headers

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Button, Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, Table } from 'react-bootstrap';
 import { FaBars, FaPlus } from 'react-icons/fa';
 
 export default function Home() {
@@ -21,7 +21,23 @@ export default function Home() {
           </Button>
         </Nav>
       </Navbar>
-      {/* Add your table code here */}
+      <Container className="my-3">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th className="table-header">Task</th>
+              <th className="table-header">Description</th>
+              <th className="table-header">Deadline</th>
+              <th className="table-header">Priority</th>
+              <th className="table-header">Is Complete</th>
+              <th className="table-header">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Add your task items here */}
+          </tbody>
+        </Table>
+      </Container>
     </>
   );
 };
